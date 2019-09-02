9f65a1b50ec2cf762c8c77dba671baa871d30727
ajilk <ajilkhomov@gmail.com>
2019-09-02 15:06:27 -0400

    Initial commit

---
+++ react-fb-tictactoe/src/Game.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Board from './components/Board'
+
+class Game extends React.Component {
+  render() {
+    return(
+      <div>
+        <Board />
+      </div>
+    );
+  }
+}
+
+export default Game
--- react-fb-tictactoe/src/index.js
+++ react-fb-tictactoe/src/index.js
@@ -1,8 +1,5 @@
-import React from 'react';
+import React from 'react'
+import Game from './Game'
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+ReactDOM.render(<Game />, document.getElementById('root'));
